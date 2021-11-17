3dfbdc3e3166e0cc9d3fc91067a2de1e1edbd050
dulcegonzalez0711 <91688348+dulcegonzalez0711@users.noreply.github.com>
2021-11-16 23:15:22 -0600

    Update ExclusivoC.java

--- ExclusivoC.java
+++ ExclusivoC.java
@@ -1,5 +1,5 @@
 /**
- * Interface que contiene los metodos exclusivos de la clase A
+ * Interface que contiene los metodos exclusivos de la clase C
  * coordenadas de su  centro.
  * @version 1.0, 16/11/2021
  * @author Dulce Gonzalez 
