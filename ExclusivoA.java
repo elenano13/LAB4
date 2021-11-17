a9199eba9a42b3c73a557b63388145a6282f85d5
dulcegonzalez0711 <91688348+dulcegonzalez0711@users.noreply.github.com>
2021-11-16 23:14:41 -0600

    Update ExclusivoA.java

--- ExclusivoA.java
+++ ExclusivoA.java
@@ -1,4 +1,14 @@
+/**
+ * Interface que contiene los metodos exclusivos de la clase A
+ * coordenadas de su  centro.
+ * @version 1.0, 16/11/2021
+ * @author Dulce Gonzalez 
+ */
+
+
 public interface ExclusivoA{
+    
+ //llama a un numero reciente con el @param num siendo el numero al que va a llamar 
     void llamarReciente(int num);
     void verTarjetas(); 
 }
