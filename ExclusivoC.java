514469ebc38cced7a3bc60b9978d585ae62b703c
dulcegonzalez0711 <91688348+dulcegonzalez0711@users.noreply.github.com>
2021-11-16 23:15:13 -0600

    Update ExclusivoC.java

--- ExclusivoC.java
+++ ExclusivoC.java
@@ -1,3 +1,9 @@
+/**
+ * Interface que contiene los metodos exclusivos de la clase A
+ * coordenadas de su  centro.
+ * @version 1.0, 16/11/2021
+ * @author Dulce Gonzalez 
+ */
 public interface ExclusivoC{
     void cambiarLlamadaEspera(int num); 
     void pronosticoTiempo();
