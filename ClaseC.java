01c6817de65d17b1491e4bfe38fc99f15432c0ce
Elena Rodriguez 21774 <88160455+elenano13@users.noreply.github.com>
2021-11-16 22:45:17 -0600

    Update ClaseC.java

--- ClaseC.java
+++ ClaseC.java
@@ -1,6 +1,11 @@
+
 import java.util.Random; 
 
-public class ClaseC extends Radio implements ExclusivoC {
+class ClaseC extends Radio implements ExclusivoC {
+	
+	public ClaseC(String modo, int volumen){
+		super(modo, volumen);
+	}
     
     public void cambiarLlamadaEspera(int num){
         System.out.println("Se ha puesto su llamada con en espera. "); 
@@ -12,9 +17,17 @@ public class ClaseC extends Radio implements ExclusivoC {
         int int_random1 = rand.nextInt(upperbound1); 
         int int_random2 = rand.nextInt (upperbound2); 
 
-        System.out.println("La temperatura mínima para hoy es de: "+ int_random1+"mientras que la máxima es de"+int_random2);
+        System.out.println("La temperatura minima para hoy es de: "+ int_random1+"mientras que la maxima es de "+int_random2);
 
 
     }
+	
+	public void TelefonoEspecial(int num){
+		cambiarLlamadaEspera(num);
+	}
+	
+	public void Productividad(){
+		pronosticoTiempo();
+	}
 
 }
