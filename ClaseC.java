d371531bca8161e3cbb39e8136fea978b04986e0
dulcegonzalez0711 <91688348+dulcegonzalez0711@users.noreply.github.com>
2021-11-16 23:12:04 -0600

    Update ClaseC.java

--- ClaseC.java
+++ ClaseC.java
@@ -1,15 +1,23 @@
-
+/**
+ * Una clase que implementa los métodos específicos para loa autos de clase A
+ * @version 1.0 15/11/2021
+ * @author Dulce Gonzalez 
+ */
 import java.util.Random; 
 
 class ClaseC extends Radio implements ExclusivoC {
 	
 	public ClaseC(String modo, int volumen){
-		super(modo, volumen);
+		super(modo, volumen); //constructor
 	}
     
     public void cambiarLlamadaEspera(int num){
         System.out.println("Se ha puesto su llamada con en espera. "); 
     } 
+	
+ /** 
+ * Genera dos valores random para las temperaturas del pronostico
+ */
     public void pronosticoTiempo(){
         Random rand = new Random(); 
         int upperbound1 = 11;
@@ -21,7 +29,10 @@ class ClaseC extends Radio implements ExclusivoC {
 
 
     }
-	
+ /** 
+ * Genera dos valores random para las temperaturas del pronostico
+ *@param num el numero al que desea llamar 
+ */
 	public void TelefonoEspecial(int num){
 		cambiarLlamadaEspera(num);
 	}
